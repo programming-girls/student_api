e7504e5bba95b1c123d16b48431c4c054ee132b8
Awinja-j <ingari61@hotmail.com>
2021-11-12 13:42:26 +0300

    move admin view

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
 from flask_login import LoginManager
 
 
@@ -9,31 +7,21 @@ from src.exam.models.model import Exam, Subject, Question, SubQuestion, Answer, 
 from src.users.models.user import User
 from src.users.models.user_class import Student, Parent
 
+#blueprints
 from src.orc_engine.ocr_server import ocr_core, ocr
 from src.users.views.user import auth
-
 from src.exam.views.text_marking_scheme import text_marking_scheme
 from src.exam.views.choice_marking_scheme import choice_marking_scheme
+from src.users.views.admin_views import ad
 
+#register blueprints
 app.register_blueprint(auth)
 app.register_blueprint(ocr)
 app.register_blueprint(text_marking_scheme)
 app.register_blueprint(choice_marking_scheme)
+app.register_blueprint(ad)
 
 login_manager = LoginManager()
-
-admin = Admin(app, name='Eshirali School', template_mode='bootstrap3')
-admin.add_view(ModelView(User, db.session))
-admin.add_view(ModelView(Exam, db.session))
-admin.add_view(ModelView(Subject, db.session))
-admin.add_view(ModelView(Question, db.session))
-admin.add_view(ModelView(SubQuestion, db.session))
-admin.add_view(ModelView(Answer, db.session))
-admin.add_view(ModelView(Image, db.session))
-admin.add_view(ModelView(Student, db.session))
-admin.add_view(ModelView(Parent, db.session))
-admin.add_view(ModelView(Exams_Done, db.session))
-admin.add_view(ModelView(Student_Answer, db.session))
 
 with app.app_context():
     from src.users.models.user import User
